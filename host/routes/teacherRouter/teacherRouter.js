3d3d98a78f70ac852d8d6c242a5372c0ea0296d4
Luu Danh Luong <phamluong6866@gmail.com>
2024-02-22 11:16:31 +0700

    update

--- host/routes/teacherRouter/teacherRouter.js
+++ host/routes/teacherRouter/teacherRouter.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import teacherController from '../../controllers/teacherController/teacherController.js';
+import express from "express";
+import teacherController from "../../controllers/teacherController/teacherController.js";
 
 const teacherRouter = express.Router();
 
 // Cập nhật để sử dụng hàm mới trong controller
-router.get('/classes', teacherController.getClassListByTeacher);
+teacherRouter.get("/classes", teacherController.getClassListByTeacher);
 
 export default teacherRouter;
