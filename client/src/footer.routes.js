ea461f557c645a677a416746a7bb2ebbeb138a4a
Luu Danh Luong <phamluong6866@gmail.com>
2024-03-06 07:55:32 +0700

    update landing pages

--- client/src/footer.routes.js
+++ client/src/footer.routes.js
@@ -4,7 +4,6 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 
-// Material Kit 2 React components
 import MKTypography from "components/MKTypography";
 
 // Images
